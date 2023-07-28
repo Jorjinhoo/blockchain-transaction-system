716516b3c36753a0fb8fc9ca54078290f4aefaf9
Oleksii Sokolov <losasokolov@gmail.com>
2023-07-28 10:05:04 +0200

    file structure changes

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 const homePage = require('../not_session_scripts/home.js');
 
-          //TO RUN THE PROGRAM, NEED TO START THE SERVER
-
 const app = express();
 const port = 5500;
 const db = 'mongodb+srv://Admin:Admin123@Accounts.ashibkj.mongodb.net/AccountsData?retryWrites=true&w=majority';
